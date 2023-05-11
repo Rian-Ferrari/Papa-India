7956a2847299f5051104ada29b486b238276ace1
Rian Ferrari <rianferrari37@gmail.com>
2023-05-10 21:33:08 -0300

    Adicionando validação de cadastro

--- Site/javascript/Cadastro.js
+++ Site/javascript/Cadastro.js
@@ -1,15 +1,39 @@
 function botaoCadastrarUsuario() {
-    var nomeDoUsuario = input_nome.value;
-    var emailDoUsuario = input_email.value;
-    var confirmarEmailDoUsuario = input_confirmarEmail.value;
-    var senhaDoUsuario = input_senha.value;
-    var confirmarSenhaDoUsuario = input_confirmarSenha.value;
+    var dadosDoUsuario = {
+        nome: input_nome.value,
+        email: input_email.value,
+        confirmarEmail: input_confirmarEmail.value,
+        senha: input_senha.value,
+        confirmarSenha: input_confirmarSenha.value
+    }
 
-    if (nomeDoUsuario != "" && nomeDoUsuario.length < 30) {
+    if (dadosDoUsuario.nome != "" && dadosDoUsuario.nome.length < 30) {
         div_cadastroDoUsuario.innerHTML = ``;
 
-        if ((emailDoUsuario != "" && emailDoUsuario.indexOf("@")) && (emailDoUsuario.indexOf(".com") || emailDoUsuario.indexOf("school"))) {
+        if (dadosDoUsuario.email.indexOf("@") && (dadosDoUsuario.email.endsWith(".com") || dadosDoUsuario.email.endsWith(".school"))) {
             div_cadastroDoUsuario.innerHTML = ``;
+
+            if  (dadosDoUsuario.confirmarEmail == dadosDoUsuario.email) {
+
+                div_cadastroDoUsuario.innerHTML = ``;
+                if  ((dadosDoUsuario.senha.length > 8 && dadosDoUsuario.senha.length) < 16 &&  dadosDoUsuario.senha.endsWith("#"))  {
+
+                    div_cadastroDoUsuario.innerHTML = ``;
+                    if  (dadosDoUsuario.confirmarSenha == dadosDoUsuario.senha)  {
+
+                        window.location.href="";
+                    }   else    {
+                        div_cadastroDoUsuario.innerHTML = `<b style="color:red;">VERIFIQUE-SE SUA SENHA FOI
+                INSERIDO CORRETAMENTE!</b>`;
+                    }
+                }   else    {
+                    div_cadastroDoUsuario.innerHTML = `<b style="color:red;">SUA SENHA DEVE CONTER 8-16 CARACTERES 
+                    E TER UM "#"!</b>`;
+                }
+            }   else    {
+                div_cadastroDoUsuario.innerHTML = `<b style="color:red;">VERIFIQUE-SE SEU E-MAIL FOI
+                INSERIDO CORRETAMENTE!</b>`;
+            }
         }   else    {
             div_cadastroDoUsuario.innerHTML = `<b style="color:red;">E-MAIL PREENCHIDO DE FORMA INCORRETA, VERIFIQUE-SE TEM "@"
             OU ".com"!</b>`;
